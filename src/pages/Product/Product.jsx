29b1807227738242f52576cfa10033fecd025225
sandeep <sandyadav434@gmail.com>
2023-07-15 17:10:43 +0530

    Slider Completed

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import './product.scss';
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  return <div>Product</div>;
+};
 
-export default Product
+export default Product;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import './products.scss';
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  return <div>Products</div>;
+};
 
-export default Products
+export default Products;
